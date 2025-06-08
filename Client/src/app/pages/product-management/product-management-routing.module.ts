3158a9037bd124395a78335b0280807809248578
Thamidu <anjanapma@gmail.com>
2025-06-08 10:17:31 +0530

    push

--- Client/src/app/pages/product-management/product-management-routing.module.ts
+++ Client/src/app/pages/product-management/product-management-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {ProductListsComponent} from "./product-lists/product-lists.component";
+import {UnitListsComponent} from "./unit-lists/unit-lists.component";
 
 const routes: Routes = [
   { path: 'list', component: ProductListsComponent },
+  { path: 'units', component: UnitListsComponent },
 ];
 
 @NgModule({
---
+++ Client/src/app/pages/product-management/unit-lists/unit-lists.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UnitListsComponent } from './unit-lists.component';
+import {ProductListsComponent} from "../product-lists/product-lists.component";
+
+describe('UnitListsComponent', () => {
+  let component: UnitListsComponent;
+  let fixture: ComponentFixture<UnitListsComponent>;
+
+  beforeEach(async () => {
+    TestBed.configureTestingModule({
+      declarations: [UnitListsComponent]
+    });
+    fixture = TestBed.createComponent(UnitListsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
